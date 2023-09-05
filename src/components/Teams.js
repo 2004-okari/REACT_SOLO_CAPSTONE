8f72d025eb01bac44ca7f4ba083bfd1506aa868e
okari <nyandika15@gmail.com>
2023-09-05 18:08:32 +0300

    redux and redux toolkit setup

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchTeams } from '../redux/teamsSlice';
 
 function Teams() {
+  const dispatch =  useDispatch()
+  const teams =  useSelector(state => state.teams.teams);
+  console.log(teams);
+
+  useEffect(() => {
+    dispatch(fetchTeams())
+  })
   return (
     <>
-
+      
     </>
   )
 }
---
+++ src/redux/teamsSlice.js
@@ -0,0 +1,38 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchTeams = createAsyncThunk('teams/fetchTeams', async () => {
+  try {
+    const response = await axios.get('https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=English%20Premier%20League');
+    console.log(response.data)
+    return response.data;
+  } catch (error) {
+    console.log('Erro getting teams:', error);
+  }
+})
+
+const initialState = {
+  teams: [],
+  loading: false,
+  error: null,
+};
+
+const teamsSlice = createSlice({
+  name:'teams',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchTeams.pending, (state) => state.loading = true)
+      .addCase(fetchTeams.fulfilled, (state, action) => {
+        state.loading = false;
+        state.teams = action.payload;
+      })
+      .addCase(fetchTeams.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      })
+  }
+})
+
+export default teamsSlice.reducer;
