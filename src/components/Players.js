d285d545c5bc273771f7455e910ad2acce2fe472
Okari Nyandika <nyandika15@icloud.com>
2023-09-06 14:18:59 +0000

    Changes in Players.js

--- src/components/Players.js
+++ src/components/Players.js
@@ -16,81 +16,83 @@ function Players() {
     dispatch(fetchTeams());
   }, [dispatch]);
 
-  return (
-    <div className="covered-div">
-      {teams && teams.length > 0 ? (
-        team ? (
-          <div className="team-div">
-            <h2 className="head-text">{team.strTeam}</h2>
-            <div className="icon-div">
-              <Link className="link" to="/">
-                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24"><path fill="currentColor" d="M19.2 2.43L16.778 0L4.8 12l11.978 12l2.422-2.43L9.653 12z" /></svg>
-              </Link>
+  let content;
+
+  if (!teams || teams.length === 0) {
+    content = <p>Loading...</p>;
+  } else if (!team) {
+    content = <p>Team not found</p>;
+  } else {
+    content = (
+      <div className="covered-div">
+        <div className="team-div">
+          <h2 className="head-text">{team.strTeam}</h2>
+          <div className="icon-div">
+            <Link className="link" to="/">
+              <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24"><path fill="currentColor" d="M19.2 2.43L16.778 0L4.8 12l11.978 12l2.422-2.43L9.653 12z" /></svg>
+            </Link>
+          </div>
+          <div className="content-div">
+            <div className="image-div">
+              <img className="image" src={team.strTeamBadge} alt={team.strTeam} />
             </div>
-            <div className="content-div">
-              <div className="image-div">
-                <img className="image" src={team.strTeamBadge} alt={team.strTeam} />
+            <div className="info-div">
+              <div className="boxes">
+                Year formed:
+                <span className="span-text">{team.intFormedYear}</span>
               </div>
-              <div className="info-div">
+              <div className="boxes">
+                Stadium:
+                <span className="span-text">{team?.strStadium}</span>
+              </div>
+              <div className="boxes">
+                Stadium capacity:
+                <span className="span-text">{team?.intStadiumCapacity}</span>
+              </div>
+              <div className="boxes">
+                Location:
+                <span className="span-text">{team?.strStadiumLocation}</span>
+              </div>
+              <div className="boxes">
+                Nickname :
+                <span className="span-text">{team?.strKeywords}</span>
+              </div>
+              {team.strKitColour1 && (
                 <div className="boxes">
-                  Year formed:
-                  <span className="span-text">{team.intFormedYear}</span>
+                  Kit color 1:
+                  <div className="span-text" style={{ backgroundColor: team.strKitColour1, width: '16px', height: '16px' }} />
                 </div>
+              )}
+              {team.strKitColour2 && (
                 <div className="boxes">
-                  Stadium:
-                  <span className="span-text">{team?.strStadium}</span>
+                  Kit color 2:
+                  <div className="span-text" style={{ backgroundColor: team.strKitColour2, width: '16px', height: '16px' }} />
                 </div>
-                <div className="boxes">
-                  Stadium capacity:
-                  <span className="span-text">{team?.intStadiumCapacity}</span>
-                </div>
-                <div className="boxes">
-                  Location:
-                  <span className="span-text">{team?.strStadiumLocation}</span>
-                </div>
-                <div className="boxes">
-                  Nickname :
-                  <span className="span-text">{team?.strKeywords}</span>
-                </div>
-                {team.strKitColour1 ? (
-                  <div className="boxes">
-                    Kit color 1:
-                    <div className="span-text" style={{ backgroundColor: team.strKitColour1, width: '16px', height: '16px' }} />
-                  </div>
-                ) : null}
-                {team.strKitColour2 ? (
-                  <div className="boxes">
-                    Kit color 2:
-                    <div className="span-text" style={{ backgroundColor: team.strKitColour2, width: '16px', height: '16px' }} />
-                  </div>
-                ) : null}
-                <div className="boxes">
-                  Teams Website:
-                  <a target="blank" className="span-text" style={{ color: 'red' }} href={team?.strWebsite}>{team.strTeam}</a>
-                </div>
-                <div className="boxes">
-                  Teams Facebook:
-                  <a target="blank" className="span-text" style={{ color: 'red' }} href={team?.strFacebook}>{team.strTeam}</a>
-                </div>
-                <div className="boxes">
-                  Teams Twitter:
-                  <a target="blank" className="span-text" style={{ color: 'red' }} href={team?.strTwitter}>{team.strTeam}</a>
-                </div>
-                <div className="boxes">
-                  Teams Instagram:
-                  <a target="blank" className="span-text" style={{ color: 'red' }} href={team?.strInstagram}>{team.strTeam}</a>
-                </div>
+              )}
+              <div className="boxes">
+                Teams Website:
+                <a target="blank" className="span-text" style={{ color: 'red' }} href={team?.strWebsite}>{team.strTeam}</a>
+              </div>
+              <div className="boxes">
+                Teams Facebook:
+                <a target="blank" className="span-text" style={{ color: 'red' }} href={team?.strFacebook}>{team.strTeam}</a>
+              </div>
+              <div className="boxes">
+                Teams Twitter:
+                <a target="blank" className="span-text" style={{ color: 'red' }} href={team?.strTwitter}>{team.strTeam}</a>
+              </div>
+              <div className="boxes">
+                Teams Instagram:
+                <a target="blank" className="span-text" style={{ color: 'red' }} href={team?.strInstagram}>{team.strTeam}</a>
               </div>
             </div>
           </div>
-        ) : (
-          <p>Team not found</p>
-        )
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
-  );
+        </div>
+      </div>
+    );
+  }
+
+  return content;
 }
 
 export default Players;
