75cc06cfe1224b3c96242fc548a8e61e28f30329
Okari Nyandika <nyandika15@icloud.com>
2023-09-06 16:29:11 +0000

    Liner fix

--- src/__test__/Navigation.test.js
+++ src/__test__/Navigation.test.js
@@ -1,11 +1,10 @@
-import React from "react";
-import { render } from '@testing-library/react';
-import Navigation from "../components/Navigation";
+import React from 'react';
 import renderer from 'react-test-renderer';
+import Navigation from '../components/Navigation';
 
-describe("Navigation displays correctly", () => {
-  test("Navigation file matches snapshot", () => {
+describe('Navigation displays correctly', () => {
+  test('Navigation file matches snapshot', () => {
     const tree = renderer.create(<Navigation />).toJSON();
     expect(tree).toMatchSnapshot();
-  })
-})
+  });
+});
--- src/__test__/Teams.test.js
+++ src/__test__/Teams.test.js
@@ -2,24 +2,25 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import configureMockStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
-import Teams from '../components/Teams'
+import Teams from '../components/Teams';
 
 const mockStore = configureMockStore([]);
 
-describe("Teams component works okay", () => {
-  test("Teams renders correctly", () => {
+describe('Teams component works okay', () => {
+  test('Teams renders correctly', () => {
     const store = mockStore({
       teams: {
         teams: [],
-      }
-    })
+      },
+    });
 
     const tree = renderer
       .create(
-      <Provider store={store}>
-        <Teams />
-      </Provider>)
+        <Provider store={store}>
+          <Teams />
+        </Provider>,
+      );
 
     expect(tree).toMatchSnapshot();
-  })
-})
+  });
+});
