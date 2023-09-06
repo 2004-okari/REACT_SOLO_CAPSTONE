af3c2b1131af1c98a276d4c8ea548760ecf4db93
Okari Nyandika <nyandika15@icloud.com>
2023-09-07 00:22:56 +0300

    Update Teams.js

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Navigation from './Navigation';
@@ -9,55 +9,71 @@ function Teams() {
   const loading = useSelector((state) => state.teams.loading);
   const error = useSelector((state) => state.teams.error);
 
+  const [searchQuery, setSearchQuery] = useState('');
+
   let content;
 
-  if (loading) {
-    content = (
-      <div className="loader">
-        <div data-glitch="Loading..." className="glitch">
-          Loading...
-        </div>
-      </div>
-    );
-  } else if (error) {
-    content = <div className="error">{error}</div>;
-  } else if (teams) {
-    content = (
-      <div className="box">
-        {teams.map((team) => (
-          <div className="box-2" key={team.idTeam}>
-            <div className="cover-div">
-              <img className="image" src={team.strTeamFanart4} alt="logo" />
-            </div>
-            <div className="inner-div">
-              <p className="text-1">{team.strTeam}</p>
-              <p className="text-2">
-                since
-                {team.intFormedYear}
-              </p>
-            </div>
-            <div className="icon-div">
-              <Link className="link" to={`/players/${team.idTeam}`}>
-                <svg
-                  className="text-3"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M4.8 21.57L7.222 24L19.2 12L7.222 0L4.8 2.43L14.347 12z"
-                  />
-                </svg>
-              </Link>
-            </div>
-          </div>
-        ))}
-      </div>
-    );
+  if (!teams || !Array.isArray(teams)) {
+    content = <p>Loading...</p>;
   } else {
-    content = null;
+    const filteredTeams = teams.filter((team) =>
+      team.strTeam.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
+    if (loading) {
+      content = (
+        <div className="loader">
+          <div data-glitch="Loading..." className="glitch">
+            Loading...
+          </div>
+        </div>
+      );
+    } else if (error) {
+      content = <div className="error">{error}</div>;
+    } else {
+      content = (
+        <div className="box">
+          <input
+            className='input-bar'
+            type="text"
+            placeholder="Search by team name"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+
+          {filteredTeams.map((team) => (
+            <div className="box-2" key={team.idTeam}>
+              <div className="cover-div">
+                <img className="image" src={team.strTeamFanart4} alt="logo" />
+              </div>
+              <div className="inner-div">
+                <p className="text-1">{team.strTeam}</p>
+                <p className="text-2">
+                  since
+                  {team.intFormedYear}
+                </p>
+              </div>
+              <div className="icon-div">
+                <Link className="link" to={`/players/${team.idTeam}`}>
+                  <svg
+                    className="text-3"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M4.8 21.57L7.222 24L19.2 12L7.222 0L4.8 2.43L14.347 12z"
+                    />
+                  </svg>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      );
+    }
   }
 
   return (
